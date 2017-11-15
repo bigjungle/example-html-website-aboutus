6f685c985391b10f90e97d17c69f3552f80049e8
fanyingjie <fanyingjie@chinazyjr.com>
2017-11-15 14:20:39 +0800

    修改link

--- 2.0_local_ZT_test/js/common/UrlLink.js
+++ 2.0_local_ZT_test/js/common/UrlLink.js
@@ -1,10 +1,8 @@
 /********************************************************************************************************************/
-var newUrl = "http://192.168.7.223:8080/v1/api/";
+var commonUrl = "http://192.168.7.223:8080/";
 var returnUrl = "http://zzt-test.chinazyjr.net/";
 var returnUrlHL = returnUrl + "html/1LoginRegister/login.html";
 var testType = "1"; /*1.测试数据 2.生产数据*/
-
-
 
 
 
@@ -22,158 +20,158 @@ var version = "1.0.0";
 var userType = "1";
 var loginUrl = "../../html/1LoginRegister/login.html";
 /*图片验证码地址*/
-var GetImageCodeUrl = newUrl + "common/getValidateImage";
+var GetImageCodeUrl = commonUrl+ "v1/api/common/getValidateImage";
 
 /*获取Token*/
-var GetTokenLink = newUrl + "common/getToken";
+var GetTokenLink = commonUrl+ "v1/api/common/getToken";
 
 /* 发送短信验证码*/
-var sendCodeMessageUrl = newUrl + "common/getSmsCode";
+var sendCodeMessageUrl = commonUrl+ "v1/api/common/getSmsCode";
 
 /* 注册*/
-var Register = newUrl + "user/register";
+var Register = commonUrl+ "v1/api/user/register";
 
 /*登录*/
-var Login = newUrl + "user/login";
+var Login = commonUrl+ "v1/api/user/login";
 
 /*忘记密码*/
-var ForgetPassword = newUrl + "user/forgetPassWord";
+var ForgetPassword = commonUrl+ "v1/api/user/forgetPassWord";
 
 /*注销*/
-var outLoginUrl = newUrl + "user/logout";
+var outLoginUrl = commonUrl+ "v1/api/user/logout";
 
 /*首页信息*/
-var indexMessageUrl = newUrl + "homePage/homePageBannerArticle";
+var indexMessageUrl = commonUrl+ "v1/api/homePage/homePageBannerArticle";
 
 /*首页列表*/
-var indexListUrl = newUrl + "homePage/homePageBorrowList";
+var indexListUrl = commonUrl+ "v1/api/homePage/homePageBorrowList";
 
 /*用户信息*/
-var UserStatusUrl = newUrl + "common/getUserStatus";
+var UserStatusUrl = commonUrl+ "v1/api/common/getUserStatus";
 
 /*账户余额*/
-var useraccountUrl = newUrl + "useraccount/homePage";
+var useraccountUrl = commonUrl+ "v1/api/useraccount/homePage";
 /*开户发短信*/
-var openSmsUrl = newUrl + "account/sendSmsCode";
+var openSmsUrl = commonUrl+ "v1/api/account/sendSmsCode";
 
 /*开户*/
-var openAccountUrl = newUrl + "account/openAccount";
+var openAccountUrl = commonUrl+ "v1/api/account/openAccount";
 
 /*开户成功查询*/
-var checkOpenAccountUrl = newUrl + "account/checkOpenAccount";
+var checkOpenAccountUrl = commonUrl+ "v1/api/account/checkOpenAccount";
 
 /*激活*/
-var toBosAcctActivateUrl = newUrl + "user/bosAcctActivate";
+var toBosAcctActivateUrl = commonUrl+ "v1/api/user/bosAcctActivate";
 /*银行卡信息*/
-var bankCardMessageUrl = newUrl + "recharge/queryBeforeRecharge";
+var bankCardMessageUrl = commonUrl+ "v1/api/recharge/queryBeforeRecharge";
 
 /**充值*/
-var rechargeUrl = newUrl + "recharge/confirmRecharge";
+var rechargeUrl = commonUrl+ "v1/api/recharge/confirmRecharge";
 
 /*充值限额*/
-var bankRchargeLimitUrl = newUrl + "recharge/queryBeforeRecharge";
+var bankRchargeLimitUrl = commonUrl+ "v1/api/recharge/queryBeforeRecharge";
 
 /**提现*/
-var toCashUrl = newUrl + "usertrade/userCash";
+var toCashUrl = commonUrl+ "v1/api/usertrade/userCash";
 
 /*提现手续费*/
-var userCashFeeUrl = newUrl + "usertrade/userCashFee";
+var userCashFeeUrl = commonUrl+ "v1/api/usertrade/userCashFee";
 
 /*提现回调*/
-var CashInfoUrl = newUrl + "usertrade/isWithdrawSuccess";
+var CashInfoUrl = commonUrl+ "v1/api/usertrade/isWithdrawSuccess";
 
 /*提现回调*/
-var CashInfoUrl = newUrl + "usertrade/isWithdrawSuccess";
+var CashInfoUrl = commonUrl+ "v1/api/usertrade/isWithdrawSuccess";
 
 /*是否激活*/
-var isBosAcctActivateUrl = newUrl + "user/isBosAcctActivate"
+var isBosAcctActivateUrl = commonUrl+ "v1/api/user/isBosAcctActivate"
 
 /*银行卡信息*/
-var cardInfoUrl = newUrl + "userQuery/queryCardInfo";
+var cardInfoUrl = commonUrl+ "v1/api/userQuery/queryCardInfo";
 
 /*用户信息查询接口*/
-var userInfoUrl = newUrl + "userQuery/queryCardInfo";
+var userInfoUrl = commonUrl+ "v1/api/userQuery/queryCardInfo";
 
 /*修改登录密码*/
-var changeLoginPasswordUrl = newUrl + "user/changePassword";
+var changeLoginPasswordUrl = commonUrl+ "v1/api/user/changePassword";
 
 /*自动投标*/
-var autoTenderUrl = newUrl + "userManage/autoTenderPlan";
+var autoTenderUrl = commonUrl+ "v1/api/userManage/autoTenderPlan";
 
 /*自动复投状态*/
-var autoTenderStatusUrl = newUrl + "userManage/queryTenderPlan";
+var autoTenderStatusUrl = commonUrl+ "v1/api/userManage/queryTenderPlan";
 
 /*换绑卡*/
-var changeCardUrl = newUrl + "account/quickBinding";
+var changeCardUrl = commonUrl+ "v1/api/account/quickBinding";
 
 /*产品列表页*/
-var ListUrl = newUrl + "product/standardAndPlanList";
+var ListUrl = commonUrl+ "v1/api/product/standardAndPlanList";
 
 /*计划标产品标题*/
-var productListUrl = newUrl + "product/productList";
+var productListUrl = commonUrl+ "v1/api/product/productList";
 
 /*出借标的详情*/
-var queryDetails = newUrl + "product/productDetail";
+var queryDetails = commonUrl+ "v1/api/product/productDetail";
 
 /*借款人信息*/
-var queryBidTabUrl = newUrl + "product/fromBorrowUserDetail";
+var queryBidTabUrl = commonUrl+ "v1/api/product/fromBorrowUserDetail";
 
 /*散标购标记录*/
-var queryInvestListUrl = newUrl + "/product/investRecord";
+var queryInvestListUrl = commonUrl+ "v1/api//product/investRecord";
 
 /*散标还款计划*/
-var payPlanUrl = newUrl + "product/getReturn";
+var payPlanUrl = commonUrl+ "v1/api/product/getReturn";
 
 /*购买*/
-var buyBidUrl = newUrl + "invest/borrowInvest";
+var buyBidUrl = commonUrl+ "v1/api/invest/borrowInvest";
 
 /*收益计算*/
-var ExpectedRevenueUrl = newUrl + "invest/getExpectedRevenue";
+var ExpectedRevenueUrl = commonUrl+ "v1/api/invest/getExpectedRevenue";
 
 /*购买记录*/
-var productManageUrl = newUrl + "useraccount/productManage";
+var productManageUrl = commonUrl+ "v1/api/useraccount/productManage";
 
 /*交易记录*/
-var transactionRecordUrl = newUrl + "useraccount/transactionRecord";
+var transactionRecordUrl = commonUrl+ "v1/api/useraccount/transactionRecord";
 
 /*账户中心卡券接口*/
-var accountCouponUrl = newUrl + "coupon/accountCoupon";
+var accountCouponUrl = commonUrl+ "v1/api/coupon/accountCoupon";
 
 /*购买页面卡券*/
-var investCouponUrl = newUrl + "coupon/investCoupon";
+var investCouponUrl = commonUrl+ "v1/api/coupon/investCoupon";
 
 /*提现卡券*/
-var withDrawCouponUrl = newUrl + "coupon/withDrawCoupon";
+var withDrawCouponUrl = commonUrl+ "v1/api/coupon/withDrawCoupon";
 
 /*电子签约*/
-var DZQYUrl = newUrl + "signing/signingContract";
+var DZQYUrl = commonUrl+ "v1/api/signing/signingContract";
 
 /*电子签约查询*/
-var QYCCUrl = newUrl + "signing/querySigningStatus";
+var QYCCUrl = commonUrl+ "v1/api/signing/querySigningStatus";
 
 /*电子合同*/
-var DZHTUrl = newUrl + "signing/downloadSigningContract";
+var DZHTUrl = commonUrl+ "v1/api/signing/downloadSigningContract";
 
 /*信息披露运营数据接口*/
-var platformConstantlyUrl = newUrl + "infoDisClosed/platformConstantly";
+var platformConstantlyUrl = commonUrl+ "v1/api/infoDisClosed/platformConstantly";
 
 /*调阅详情-当前持有债券*/
-var dqDebtBillsUrl = newUrl + "debt/cash/held";
+var dqDebtBillsUrl = commonUrl+ "v1/api/debt/cash/held";
 
 /*资产配置历史*/
-var zcpzAssetConfigLogUrl = newUrl + "debt/cash/match";
+var zcpzAssetConfigLogUrl = commonUrl+ "v1/api/debt/cash/match";
 
 /*债权转让记录*/
-var zqTransferRecordsUrl = newUrl + "debt/cash/transfer";
+var zqTransferRecordsUrl = commonUrl+ "v1/api/debt/cash/transfer";
 
 /*资金流水*/
-var FundsAccountsUrl = newUrl + "debt/cash/fund/flow";
+var FundsAccountsUrl = commonUrl+ "v1/api/debt/cash/fund/flow";
 
 /*出借详情*/
-var FundDetailsUrl = newUrl + "debt/invest/detail";
+var FundDetailsUrl = commonUrl+ "v1/api/debt/invest/detail";
 
 /*下载合同*/
-var XZHTUrl = newUrl + "signing/downloadSigningContract";
+var XZHTUrl = commonUrl+ "v1/api/signing/downloadSigningContract";
 
 /*预览合同*/
-var YLHTUrl = newUrl + "signing/previewSigningContract";
+var YLHTUrl = commonUrl+ "v1/api/signing/previewSigningContract";

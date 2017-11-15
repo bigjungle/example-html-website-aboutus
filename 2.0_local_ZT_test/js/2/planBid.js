3c33a0c83d411a8a4619a4ebfaa97a02e43a5575
fanyingjie <fanyingjie@chinazyjr.com>
2017-11-15 11:56:11 +0800

    修改  icon

--- 2.0_local_ZT_test/js/2/planBid.js
+++ 2.0_local_ZT_test/js/2/planBid.js
@@ -30,7 +30,7 @@ $(function() {
 				$(".tiltle").append(ctc);
 				break;
 			case 1:
-				var ctc = '您所在的位置：<a href=""><span>首页</span></a>> <span>计划标</span>> <span>赢计划</span>';
+				var ctc = '您所在的位置：<a href=""><span>首页</span></a>> <span>计划标</span>> <span>散标</span>';
 				$(".tiltle").append(ctc);
 				$(".planBidList1").show();
 				$(".planBidList0").hide();

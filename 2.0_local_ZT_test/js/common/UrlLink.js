72543ea254ee4db1f25fdd2631b1cd4acb140914
fanyingjie <fanyingjie@chinazyjr.com>
2018-01-24 09:37:40 +0800

    修改活动  代码

--- 2.0_local_ZT_test/js/common/UrlLink.js
+++ 2.0_local_ZT_test/js/common/UrlLink.js
@@ -1,7 +1,7 @@
 /********************************************************************************************************************/
 //var commonUrl = "http://hapi.chinazyjr.com/";
 var commonUrl = "http://api-testgl.chinazyjr.net/";
-var returnUrl = "http://zzt-testd.chinazyjr.com/";
+var returnUrl = "http://zzt-testgl.chinazyjr.net/";
 var returnUrlHL = returnUrl + "html/1LoginRegister/login.html";
 var testType = "1"; /*1.测试数据 2.生产数据*/
 

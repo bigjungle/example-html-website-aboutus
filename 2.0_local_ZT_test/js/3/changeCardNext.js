18407d3f3f163e705996a52da9c076bdddb82508
fanyingjie <fanyingjie@chinazyjr.com>
2018-02-28 16:13:42 +0800

    增加四家开户银行

--- 2.0_local_ZT_test/js/3/changeCardNext.js
+++ 2.0_local_ZT_test/js/3/changeCardNext.js
@@ -13,18 +13,18 @@ $(function() {
 	$(".bankLogoForm").html("");
 	for(var i = 100; i < 125; i++) {
 		switch(i) {
-			case 101:
+//			case 101:
 			case 104:
 			case 105:
 			case 108:
-			case 110:
+//			case 110:
 			case 113:
 			case 114:
 			case 116:
-			case 117:
+//			case 117:
 			case 118:
 			case 119:
-			case 122:
+//			case 122:
 			case 124:
 				break;
 			default:
